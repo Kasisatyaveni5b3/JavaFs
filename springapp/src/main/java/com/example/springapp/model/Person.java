fd16c2ee61e1be55fcfc268e61a82d729aa48986
neojarvis <techadmin@iamneo.ai>
2026-01-30 05:16:50 +0000

    Auto-save-commit-30/01/2026--10:46:38

--- springapp/src/main/java/com/example/springapp/model/Person.java
+++ springapp/src/main/java/com/example/springapp/model/Person.java
@@ -1,5 +1,31 @@
 package com.example.springapp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String address;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public Person(int id, String address) {
+        this.id = id;
+        this.address = address;
+    }
+    
     
 }
