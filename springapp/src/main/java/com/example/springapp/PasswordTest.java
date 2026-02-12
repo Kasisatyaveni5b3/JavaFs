f9de15d4af00b1b9d9781facd467edbe58cfb433
neojarvis <techadmin@iamneo.ai>
2026-02-12 08:09:25 +0000

    Auto-save-commit-12/02/2026--13:39:05

--- springapp/src/main/java/com/example/springapp/PasswordTest.java
+++ springapp/src/main/java/com/example/springapp/PasswordTest.java
@@ -1,7 +1,12 @@
 package com.example.springapp;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class PasswordTest{
     public static void main(String[] args) {
-        Bc
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("12345"));
+        System.out.println(encoder.encode("12345"));
+
     }
 }
--- springapp/src/main/java/com/example/springapp/SecurityConfig.java
+++ springapp/src/main/java/com/example/springapp/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,5 +20,16 @@ public class SecurityConfig {
     .authorizeHttpRequests().antMatchers("/api/public").permitAll()
     .anyRequest().authenticated().and().httpBasic();
     return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public UserDetailsService userDetailService() {
+    UserDetails user = User.builder();
+    
   }
 }
