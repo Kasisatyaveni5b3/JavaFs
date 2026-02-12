5d6fdb3abf75c0500c407438f8e226fc3f885ce4
neojarvis <techadmin@iamneo.ai>
2026-02-12 07:12:55 +0000

    Auto-save-commit-12/02/2026--12:42:36

--- springapp/src/main/java/com/example/springapp/SecurityConfig.java
+++ springapp/src/main/java/com/example/springapp/SecurityConfig.java
@@ -2,13 +2,12 @@ package com.example.springapp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-      http.csrf(csrf -> csrf.disable());
-      authorizeHttpRequests(auth-> auth.)
-    }
+@EnableWebSecurity
+public class SecurityConfig extends Se {
+  @Bean
+  
 }
---
+++ springapp/src/main/java/com/example/springapp/controller/SecurityController.java
@@ -0,0 +1,21 @@
+package com.example.springapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class SecurityController {
+    
+    @PostMapping("/public")
+    public String publicMethod() {
+        return "this is public method";
+    }
+
+    @GetMapping("/private")
+    public String privateMethod() {
+        return "this is private method";
+    }
+}
