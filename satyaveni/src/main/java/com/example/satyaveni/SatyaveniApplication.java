faafa6f26c676b490529b2cd138d7d5dd801d407
neojarvis <techadmin@iamneo.ai>
2026-02-14 05:44:34 +0000

    Auto-save-commit-14/02/2026--11:14:14

---
+++ satyaveni/src/main/java/com/example/satyaveni/SatyaveniApplication.java
@@ -0,0 +1,13 @@
+package com.example.satyaveni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatyaveniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatyaveniApplication.class, args);
+	}
+
+}
---
+++ satyaveni/src/main/java/com/example/satyaveni/model/Employee.java
@@ -0,0 +1,62 @@
+package com.example.satyaveni;
+
+@Entity
+public class Employee {
+    private int id;
+    private String name;
+    private String email;
+    private String designation;
+    private String phoneNumber;
+
+    public Employee() {
+
+    }
+
+    public Employee(String name, String email, String designation, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.designation = designation;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
