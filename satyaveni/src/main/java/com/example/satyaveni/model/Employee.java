050f95a7b9c8db947400aa2300b356c1b023dab1
neojarvis <techadmin@iamneo.ai>
2026-02-15 15:37:32 +0000

    Auto-save-commit-15/02/2026--21:07:31

--- satyaveni/src/main/java/com/example/satyaveni/model/Employee.java
+++ satyaveni/src/main/java/com/example/satyaveni/model/Employee.java
@@ -1,7 +1,11 @@
-package com.example.satyaveni;
+package com.example.satyaveni.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
 public class Employee {
+    @Id
     private int id;
     private String name;
     private String email;
