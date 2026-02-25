3210b7dca7d0d58bf91565de9d89707333a993b5
neojarvis <techadmin@iamneo.ai>
2026-02-25 05:33:26 +0000

    Auto-save-commit-2/25/2026--11:03:25-AM

--- backup.sql
+++ backup.sql
@@ -123,6 +123,31 @@ LOCK TABLES `todo_list` WRITE;
 INSERT INTO `todo_list` VALUES (1,'2026-02-10 08:52:00.948639','completed','implementing todolist. now created post method','todolist','2026-02-10 09:30:42.102053'),(3,'2026-02-10 08:53:57.048699','pending','implementing todolist. now created post method','todolist',NULL),(4,'2026-02-10 09:09:26.195225','inpending','created todolist','todolist',NULL),(5,'2026-02-10 09:09:52.207560','inpending','created todolist','todolist',NULL),(6,'2026-02-10 09:11:33.069066','inpending','created todolist','todolist',NULL);
 /*!40000 ALTER TABLE `todo_list` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -133,4 +158,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-02-25  5:18:26
+-- Dump completed on 2026-02-25  5:33:26
---
+++ springapp/src/main/java/com/example/springapp/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.example.springapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthController {
+    @Autowired
+    UserRe
+}
