275989b9a7b57fe437756b2e0e1e6fd1198bed51
neojarvis <techadmin@iamneo.ai>
2026-02-25 10:00:55 +0000

    Auto-save-commit-2/25/2026--3:30:55-PM

---
+++ springapp/src/main/java/com/example/springapp/JwtFilter.java
@@ -0,0 +1,28 @@
+package com.example.springapp;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.springapp.service.UserService;
+import com.example.springapp.utils.JwtUtil;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    JwtUtil jwtUtil;
+
+    @Autowired
+    UserService userService;
+
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filter) {
+       String authenticationHeader = request.getHeader("AUTHORIZATION");
+       if(authenticationHeader != null && authenticationHeader.startsWith("Bearer")) {
+        String token = authenticationHeader.substring(7);
+       }
+    }
+}
