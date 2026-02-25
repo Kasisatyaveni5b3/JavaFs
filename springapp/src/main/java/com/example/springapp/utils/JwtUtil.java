9738f4547b6fe8be8d9e33a006eaf08469d93f7d
neojarvis <techadmin@iamneo.ai>
2026-02-25 07:09:44 +0000

    Auto-save-commit-2/25/2026--12:39:43-PM

--- springapp/src/main/java/com/example/springapp/utils/JwtUtil.java
+++ springapp/src/main/java/com/example/springapp/utils/JwtUtil.java
@@ -6,11 +6,18 @@ import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
     private String mySecret="mySuperSecretKey12345678901234567890";
     private String expirationDate="3600000";
-
     
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(mySecret.getBytes());
+    }
+    
+    public String generateToken(String email) {
+       return Jwts.builder().setSubject
+    }
 }
