6987492cb56933e7835ea38885e6d49dabe0370d
neojarvis <techadmin@iamneo.ai>
2026-02-26 06:53:46 +0000

    implemented jwt

--- springapp/src/main/java/com/example/springapp/controller/AuthController.java
+++ springapp/src/main/java/com/example/springapp/controller/AuthController.java
@@ -8,12 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springapp.model.User;
 import com.example.springapp.repository.UserRepo;
 import com.example.springapp.service.UserService;
 
-@Controller
+@RestController
 public class AuthController {
     @Autowired
     UserService userService;
